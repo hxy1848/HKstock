c794a24a99daece35ffac8c9bc40a41886d03245
hxy1848 <244992140@qq.com>
2021-07-28 11:16:11 +0800

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import requests
+
+
+"""
+个人股票投资分析，指标为北向资金动态，分析北向趋势，针对个股进行投资建议。
+1、数据源：港交所披露易：https://sc.hkexnews.hk/TuniS/www.hkexnews.hk/index_c.htm
+2、主要指标：上一日
+3、形成策略：
+"""
---
+++ readme.md
@@ -0,0 +1,6 @@
+# 北向资金爬虫
+
+主要爬取北向资金流向，对个股当日，上周，三十日内进行分析。
+
+数据源头：https://sc.hkexnews.hk/TuniS/www.hkexnews.hk/index_c.htm
+
